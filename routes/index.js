0ccaba3cbd2b7479de8beba2fa112a5bf6aab4b9
Firas1993 <firaskebsi@gmail.com>
2019-01-05 10:09:40 +0100

    create te twitter search files 2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
+var twitterSearch = require("../logic/twitterSearch");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'ExpressFirasKebsiJS' });
 });
 
-module.exports = router;
+router.post('/search', function (req, res) {
+  twitterSearch(req.body.search, function (data) {
+    res.json(data);
+  });
+});
+  module.exports = router;
